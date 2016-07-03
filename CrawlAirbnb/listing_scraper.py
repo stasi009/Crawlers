2375497cd40440e304d161976cd56291b095e5af
Stasi <cheka008@gmail.com>
2016-07-03 11:43:34 -0700

    add logger

--- CrawlAirbnb/listing_scraper.py
+++ CrawlAirbnb/listing_scraper.py
@@ -7,6 +7,7 @@ import re
 import itertools
 import threading
 import time
+import logging
 from abn_entities import Room
 
 def is_english(txt):
@@ -34,77 +35,94 @@ def get_review_tags(soup,meta):
     else:
         return None
 
-def get_evaluations(room,meta):
-    response = requests.get("https://www.airbnb.com/rooms/{}".format(room.id))
-    soup = BeautifulSoup(response.text)
-    
-    tag = soup.find('script',{'type':"application/json",'data-hypernova-key':"listingbundlejs"})
-    # json text is embedded in '<!--xxx-->', so remove comment tags at both ends
-    d = json.loads( tag.text[4:-3] )
-
-    ################ star distribution
-    dict_star_histogram = d["starHistogramData"]
-    for s in dict_star_histogram:
-        r = int( s["rating"])
-        # rating is from 1 to 5, so have to r-1
-        room.star_percentages[r-1] = s["percentage"]
-
-    dict_listing = d["listing"]
-    ################ retrieve aspect ratings
-    room.review_score = dict_listing["review_details_interface"]["review_score"]
-
-    review_summary = dict_listing["review_details_interface"]["review_summary"]
-    for aspect in review_summary:
-        room.aspect_ratings[aspect["label"]] = aspect["value"] 
-
-    ################ retrieve review tags
-    review_tags = dict_listing["listing_tags"]
-    for tag in review_tags:
-        tagid = tag["tagId"]
-        # order is important, only show first 5 on the page
-        # so the first 5 must be the most important 5 aspects
-        room.review_tags.append(meta.tagid2txt(tagid))
-
-    ################ how many save this room into their wishlist
-    room.saved2wishlist = get_saved2wishlist(soup,room.id)
-
-def get_onepage_comments(listingid,offset,limits=50):
-    api_url = "https://api.airbnb.com/v2/reviews?client_id=3092nxybyb0otqw18e8nh5nty&role=all&listing_id={}&_offset={}&_limit={}".format(listingid,offset,limits)
-
-    response = requests.get(api_url)
-    # if there is no reviews, response still have 'reviews' section, but just empty
-    reviews = response.json()["reviews"]
-
-    eof = True if len(reviews) < limits else False
-    return eof,(review["comments"] for review in reviews if is_english(review["comments"]) )
-
 class ListingScraper(threading.Thread):
     def __init__(self,meta,inQ,outQ,settings = {}):
         threading.Thread.__init__(self)
         self._meta = meta
         self._inQ = inQ
         self._outQ = outQ
-
+        
+        self._logger = logging.getLogger("crawlabn.listing")
         self._sleep_interval = settings.get("sleep_interval",2)# unit: seconds
         self._page_limits = settings.get("page_limits",50)# maximal size in a page
+
+    def get_evaluations(self,room):
+        response = requests.get("https://www.airbnb.com/rooms/{}".format(room.id))
+        if response.ok:
+            self._logger.info("listing<%d>'s webpage downloaded",room.id)
+
+            soup = BeautifulSoup(response.text)
+            tag = soup.find('script',{'type':"application/json",'data-hypernova-key':"listingbundlejs"})
+            # json text is embedded in '<!--xxx-->', so remove comment tags at both ends
+            d = json.loads( tag.text[4:-3] )
+
+            ################ star distribution
+            dict_star_histogram = d["starHistogramData"]
+            for s in dict_star_histogram:
+                r = int( s["rating"])
+                # rating is from 1 to 5, so have to r-1
+                room.star_percentages[r-1] = s["percentage"]
+
+            dict_listing = d["listing"]
+            ################ retrieve aspect ratings
+            room.review_score = dict_listing["review_details_interface"]["review_score"]
+
+            review_summary = dict_listing["review_details_interface"]["review_summary"]
+            for aspect in review_summary:
+                room.aspect_ratings[aspect["label"]] = aspect["value"] 
+
+            ################ retrieve review tags
+            review_tags = dict_listing["listing_tags"]
+            for tag in review_tags:
+                tagid = tag["tagId"]
+                # order is important, only show first 5 on the page
+                # so the first 5 must be the most important 5 aspects
+                room.review_tags.append(self._meta.tagid2txt(tagid))
+
+            ################ how many save this room into their wishlist
+            room.saved2wishlist = get_saved2wishlist(soup,room.id)
+        else:
+            self._logger.error("failed to download listing<%d>'s webpage, status code=%d, reason='%s', content='%s'",room.id, response.status_code,response.reason,response.text)
+            response.raise_for_status()
+
+    def get_onepage_comments(self,listingid,offset,limits=50):
+        api_url = "https://api.airbnb.com/v2/reviews?client_id=3092nxybyb0otqw18e8nh5nty&role=all&listing_id={}&_offset={}&_limit={}".format(listingid,offset,limits)
+        response = requests.get(api_url)
+
+        if response.ok:
+            self._logger.info("listing<%d>'s comments from [%d~%d] downloaded",listingid,offset,offset+limits)
+
+            # if there is no reviews, response still have 'reviews' section, but just empty
+            reviews = response.json()["reviews"]
+
+            eof = True if len(reviews) < limits else False
+            return eof,(review["comments"] for review in reviews if is_english(review["comments"]) )
+        else:
+            self._logger.error("failed to download listing<%d>'s comments from [%d~%d] downloaded, status code=%d, reason='%s', content='%s'",listingid,offset,offset+limits, response.status_code,response.reason,response.text)
+            response.raise_for_status()
 
     def get_room(self,listing_id):
         #################### basic information
         time.sleep(self._sleep_interval)
         api_url = "https://api.airbnb.com/v2/listings/{}?client_id=3092nxybyb0otqw18e8nh5nty&_format=v1_legacy_for_p3".format(listing_id)
         response = requests.get(api_url)
-        room = Room(response.json()["listing"])
+        if response.ok:
+            self._logger.info("JSON downloaded from listing<%d>'s API",listing_id)
+            room = Room(response.json()["listing"])
+        else:
+            self._logger.error("failed to download listing<%d>'s from API, status code=%d, reason='%s', content='%s'",listingid,offset,offset+limits, response.status_code,response.reason,response.text)
+            response.raise_for_status()
 
         #################### aspect ratings
         time.sleep(self._sleep_interval)
-        get_evaluations(room,self._meta)
+        self.get_evaluations(room)
 
         #################### comments
         offset = 0
         eof = False
         while not eof:
             time.sleep(self._sleep_interval)
-            eof,comments = get_onepage_comments(room.id,offset,self._page_limits)
+            eof,comments = self.get_onepage_comments(room.id,offset,self._page_limits)
             room.comments.extend(comments)
             offset += self._page_limits
 
--- CrawlAirbnb/tests.py
+++ CrawlAirbnb/tests.py
@@ -8,18 +8,23 @@ from listing_scraper import ListingScraper
 from listing_saver import ListingJsonSaver
 from meta_info import MetaInfo
 
-logger = logging.getLogger('')# root logger
+logger = logging.getLogger('crawlabn')
 logger.setLevel(logging.INFO)
 
 
 meta = MetaInfo()
 listing_scraper = ListingScraper(meta,None,None)
 
-listing_id = 1485746
+listing_id = 25002
 room = listing_scraper.get_room(listing_id)
 
 saver = ListingJsonSaver("rooms_json")
 saver.save(room)
+
+
+
+url = "https://api.airbnb.com/v2/reviews?client_id=3092nxybyb0otqw18e8nh5nty&listing_id=2056659&role=all&_limit=60"
+r = requests.get(url)
 
 
 
