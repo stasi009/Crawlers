519da2e48a11e2b75772fb896079455912c0bc41
Stasi <cheka008@gmail.com>
2016-07-03 10:39:40 -0700

    if outside USA, zipcode may not be integer

--- CrawlAirbnb/tests.py
+++ CrawlAirbnb/tests.py
@@ -5,6 +5,7 @@ import json
 import logging
 from abn_entities import Room
 from listing_scraper import ListingScraper
+from listing_saver import ListingJsonSaver
 from meta_info import MetaInfo
 
 logger = logging.getLogger('')# root logger
@@ -16,6 +17,9 @@ listing_scraper = ListingScraper(meta,None,None)
 
 listing_id = 1485746
 room = listing_scraper.get_room(listing_id)
+
+saver = ListingJsonSaver("rooms_json")
+saver.save(room)
 
 
 
